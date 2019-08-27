bf58e955118bda91c6471fb0d98642bef7703915
adahra <adnantoa@gmail.com>
2019-08-27 16:33:55 +0700

    This is first commit

---
+++ src/main/java/io/github/adahra/JimpitanApplication.java
@@ -0,0 +1,13 @@
+package io.github.adahra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimpitanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JimpitanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/adahra/JimpitanApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.adahra;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JimpitanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
